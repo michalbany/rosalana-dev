a0352f113fc2ecf4f5b0d839cad2f11d94dfc518
michalbany <xbanym00@vutbr.cz>
2024-09-13 20:34:39 +0200

    done

--- app.config.ts
+++ app.config.ts
@@ -2,11 +2,11 @@
 export default defineAppConfig({
   docus: {
     title: 'Rosalana Dev',
-    description: 'The best place to start your documentation.',
+    description: 'My awesome library of components',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
-      twitter: 'nuxt_js',
-      github: 'nuxt-themes/docus',
+      twitter: 'michal_bany',
+      github: 'michalbany/rosalana-dev',
       nuxt: {
         label: 'Nuxt',
         icon: 'simple-icons:nuxtdotjs',
@@ -14,11 +14,11 @@ export default defineAppConfig({
       }
     },
     github: {
-      dir: '.starters/default/content',
-      branch: 'main',
-      repo: 'docus',
-      owner: 'nuxt-themes',
-      edit: true
+      dir: '.',
+      branch: 'master',
+      repo: 'rosalana-dev',
+      owner: 'michalbany',
+      edit: false
     },
     aside: {
       level: 0,
